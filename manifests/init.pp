d0cb2e86ff0e558d1292974ff2bd0f7cae2534c6
hypervci <hyper-v_ci@microsoft.com>
2016-03-15 17:45:12 +0000

    lint cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,7 +36,7 @@ class windows_sensu (
   $rabbitmq_vhost           = '/sensu',
   $rabbitmq_ssl_private_key = '/etc/sensu/ssl/key.pem',
   $rabbitmq_ssl_cert_chain  = '/etc/sensu/ssl/cert.pem',
-  $subscriptions            = ["hyper-v"],
+  $subscriptions            = ['hyper-v'],
   $client_name              = $::fqdn,
   $client_custom            = {},
   $safe_mode                = false,
@@ -53,59 +53,52 @@ class windows_sensu (
       before      => Package[$package],
     }
   }
- 
   windows_common::configuration::feature { 'NET-Framework-Core':
     ensure   => present,
-  } 
-
+  }
   package { $package:
     ensure          => installed,
     source          => $sensu_installer_path,
     install_options => ['/passive'],
     require         => Windows_Common::Configuration::Feature['NET-Framework-Core'],
   }
-  
   file_line { 'sensu_conf_arg':
-    path    => 'c:/opt/sensu/bin/sensu-client.xml',
-    match   => "<arguments*",
-    line    => "<arguments>C:\\opt\\sensu\\embedded\\bin\\sensu-client -d C:\\etc\\sensu\\conf.d -l C:\\opt\\sensu\\sensu-client.log</arguments>",
     ensure  => present,
+    path    => 'c:/opt/sensu/bin/sensu-client.xml',
+    match   => '<arguments*',
+    line    => '<arguments>C:\\opt\\sensu\\embedded\\bin\\sensu-client -d C:\\etc\\sensu\\conf.d -l C:\\opt\\sensu\\sensu-client.log</arguments>',
     require => Package[$package],
     before  => Exec['sc_create_sensu_service'],
   }
- 
   file{'c:/etc': ensure  => directory, }
   file{'c:/etc/sensu': ensure => directory, }
   file{'c:/etc/sensu/ssl': ensure => directory,}
   file{'c:/etc/sensu/conf.d': ensure => directory, }
   File['c:/etc'] -> File['c:/etc/sensu'] -> File['c:/etc/sensu/ssl'] ->  File['c:/etc/sensu/conf.d']
- 
+
   file { 'c:/etc/sensu/ssl/cert.pem':
-    ensure  => present,
-    source  => "puppet:///extra_files/sensu/cert.pem",
+    ensure  => file,
+    source  => 'puppet:///extra_files/sensu/cert.pem',
     require => File['c:/etc', 'c:/etc/sensu', 'c:/etc/sensu/conf.d', 'c:/etc/sensu/ssl'],
   }
 
   windows_common::remote_file{'key.pem':
-    source      => "http://10.21.7.22/sensu/key.pem",
+    source      => 'http://10.21.7.22/sensu/key.pem',
     destination => 'c:/etc/sensu/ssl/key.pem',
     require     => File['c:/etc', 'c:/etc/sensu', 'c:/etc/sensu/conf.d', 'c:/etc/sensu/ssl'],
   }
- 
   #file { 'c:/etc/sensu/ssl/key.pem':
-   # ensure  => present,
+    #ensure  => present,
     #source  => "puppet:///extra_files/sensu/key.pem",
     #source   => "http:///10.21.7.22/tim/sensu/key.pem",
     #require => File['c:/etc', 'c:/etc/sensu', 'c:/etc/sensu/conf.d', 'c:/etc/sensu/ssl'],
     #source_permissions  => ignore,
   #}
-
   file { 'c:/etc/sensu/conf.d/rabbitmq.json':
     ensure  => present,
     before  => Sensu_rabbitmq_config[$::fqdn],
     require => File['c:/etc', 'c:/etc/sensu', 'c:/etc/sensu/conf.d', 'c:/etc/sensu/ssl'],
   }
-    
   sensu_rabbitmq_config { $::fqdn:
     ensure          => present,
     port            => $windows_sensu::rabbitmq_port,
@@ -116,17 +109,13 @@ class windows_sensu (
     ssl_cert_chain  => $windows_sensu::rabbitmq_ssl_cert_chain,
     ssl_private_key => $windows_sensu::rabbitmq_ssl_private_key,
   }
-  
   file { 'c:/etc/sensu/conf.d/client.json':
-    ensure  => $ensure,
+    ensure  => $::ensure,
     before  => Sensu_client_config[$::fqdn],
     require => File['c:/etc', 'c:/etc/sensu', 'c:/etc/sensu/conf.d', 'c:/etc/sensu/ssl'],
   }
-
   $client_address  = inline_template("<%= `nslookup ${::fqdn} | grep '10.21.7' | cut -d ':' -f2 |  tr '\n' '\t' | sed 's/^[ \t]*//;s/[ \t]*$//'` -%>")
-
   notify { "client address is ${client_address}": }
-
   sensu_client_config { $::fqdn:
     ensure        => present,
     client_name   => $windows_sensu::client_name,
@@ -135,21 +124,17 @@ class windows_sensu (
     safe_mode     => $windows_sensu::safe_mode,
     custom        => $windows_sensu::client_custom,
   }
-  
   exec { 'sc_create_sensu_service':
     command   => 'sc.exe create sensu-client start= delayed-auto binPath= c:\opt\sensu\bin\sensu-client.exe DisplayName= "Sensu Client"',
-    path      => "${path}",
+    path      => $path,
     logoutput => true,
-    require   => [Package[$package],File['c:/etc/sensu/conf.d/client.json'],File['c:/etc/sensu/conf.d/rabbitmq.json']],
+    require   => [ Package[$package],File['c:/etc/sensu/conf.d/client.json','c:/etc/sensu/conf.d/rabbitmq.json']],
     unless    => 'sc.exe getdisplayname sensu-client',
   }
-
   service { 'sensu-client':
     ensure    => running,
     enable    => true,
     require   => Exec[sc_create_sensu_service],
     subscribe => [ File['c:/etc/sensu/conf.d/client.json'],File['c:/etc/sensu/conf.d/rabbitmq.json'] ],
   }
-
 }
-
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,4 @@
+# == Class::windows_sensu::params
 class windows_sensu::params {
   $url     = 'http://repos.sensuapp.org/msi/sensu-0.12.5-1.msi'
   $package = 'Sensu'
